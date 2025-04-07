9f98f003e82b3769a09a36a7c9e89aa8ebf584fa
Paine12 <38664927+Paine12@users.noreply.github.com>
2025-04-07 22:57:01 +0300

    Random baloons

---
+++ script.js
@@ -0,0 +1,21 @@
+let body = document.querySelector(".ecran")
+let baloon = document.querySelector(".baloon")
+
+function animationFinish(n){
+    Promise.all(n.getAnimations().map((animation) => animation.finished)).then(
+        () => n.remove(),
+      ); 
+    
+}
+
+animationFinish(baloon)
+
+function moreBaloons() {
+    let balonNou = baloon.cloneNode(true)
+    balonNou.style.width = `${Math.floor(Math.random() * 100) + 100}px`
+    balonNou.style.left = `${Math.floor(Math.random() * 100)}%`
+    document.body.appendChild(balonNou)
+    animationFinish(balonNou)
+}
+
+setInterval(moreBaloons,(3000));
